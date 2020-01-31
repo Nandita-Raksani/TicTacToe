b5bcb726529610e07dff98ad44f30079d9395af5
Nandita <nrak15@gmail.com>
2020-02-01 04:00:04 +0530

    Restructured to accommodate the winner check for upper right to lower left diagonal to happen in a single condition

--- src/main/java/com/kata/practise/TicTacToe.java
+++ src/main/java/com/kata/practise/TicTacToe.java
@@ -69,9 +69,7 @@ public class TicTacToe {
     }
     private boolean isWinIfUpperRightToLowerLeftDiagonalOccupiedWithSameSymbol() {
         char[][] gridlayout = gameBoard.getLayout();
-        if (gridlayout[0][2] != '\0' && gridlayout[0][2] == SYMBOL_X && gridlayout[1][1] == SYMBOL_X && gridlayout[2][0] == SYMBOL_X) {
-            return true;
-        } else if (gridlayout[0][2] != '\0' && gridlayout[0][2] == SYMBOL_O && gridlayout[1][1] == SYMBOL_O && gridlayout[2][0] == SYMBOL_O) {
+        if (gridlayout[0][2] != '\0' && gridlayout[0][2] == gridlayout[1][1] && gridlayout[1][1] == gridlayout[2][0]) {
             return true;
         }
         return false;
