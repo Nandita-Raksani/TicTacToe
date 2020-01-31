9d2633245742f18069c4277f5fd8a694bfc6bc61
Nandita <nrak15@gmail.com>
2020-02-01 01:50:46 +0530

    Extracted the method initialization of TicTacToe to a reusable method

--- src/test/java/com/kata/practise/TicTacToeTest.java
+++ src/test/java/com/kata/practise/TicTacToeTest.java
@@ -1,25 +1,30 @@
 package com.kata.practise;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TicTacToeTest {
+    TicTacToe ticTacToe;
+
+    @Before
+    public void shouldInitializeTicTacToe(){
+        ticTacToe = new TicTacToe();
+    }
+
     @Test
     public void newGameInstanceShouldNotBeNull() {
-        TicTacToe ticTacToe = new TicTacToe();
         assertNotNull(ticTacToe);
     }
 
     @Test
     public void shouldStartGameWithAnEmptyBoard(){
-        TicTacToe ticTacToe = new TicTacToe();
         assertTrue(ticTacToe.isVacant());
     }
 
     @Test
     public void shouldAssignFirstTurnAsX(){
-        TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.play(0,0);
         assertEquals('X', ticTacToe.getSymbolAt(0,0));
     }
